7ac8e27de0830b69642d7878e42213eef500b95c
Damien Pinna <damien.pinna@gmail.com>
2020-05-28 18:29:27 +0200

    création de la page d'accueil statique

--- query.js
+++ query.js
@@ -2,9 +2,9 @@
  * Fonction qui permet de demander à l'API tous les oursons
  * @returns {Object}
  */
-const getTeddies = async () => {
+const getTeddies = async () => { //Fonction asynchrone
    try { //On controle si la requête fonctionne bien
-      let response = await fetch('http://localhost:3000/api/teddies'); //On fait une requête sur cet URL et on attend une réponse en JSON
+      let response = await fetch('http://localhost:3000/api/teddies'); //On fait une requête sur cet URL et on attend une réponse en JSON (retourne une promesse)
       if (response.ok) { //On test si oui ou nous nous avons eu un retour correct (statut HTTP entre 200 et 299)
          let data = await response.json(); //On assigne la réponse convertit en objet JavaScript à la variable data (la méthode body.json renvoie une promesse)
          console.log(data);

3d91f110af644020ef5ca00619d56d88c1508b70
Damien Pinna <damien.pinna@gmail.com>
2020-06-01 11:04:11 +0200

    la fonction afficherTeddies affiche désormais tous les teddies

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const afficherTeddies = async () => {
    const data = await getTeddies();
-   const card = document.querySelector('.card');
-   console.log(data)
-   card.innerHTML = `<a href="#"><img class="card-img-top" src="${data[0].imageUrl}" alt=""></a>
-                     <div class="card-body">
-                        <h4 class="card-title">
-                           <a href="#">${data[0].name}</a>
-                        </h4>
-                        <h5>$${data[0].price}</h5>
-                        <p class="card-text">${data[0].description}</p>
-                     </div>
-                     <div class="card-footer text-center">
-                        <button type="button" class="btn btn-secondary">Commander</button>
-                     </div>`
+   const articles = document.querySelector('#articles');
+   console.log(data[0])
+   let teddies = ""; 
+   for (let teddy of data) {
+      teddies +=`
+            <div class="col-lg-4 col-md-6 mb-4">
+               <div class="card h-100">
+                  <a href="#"><img class="card-img-top" src="${teddy.imageUrl}" alt=""></a>
+                  <div class="card-body">
+                     <h4 class="card-title">
+                        <a href="#">${teddy.name}</a>
+                     </h4>
+                     <h5>$${teddy.price}</h5>
+                     <p class="card-text">${teddy.description}</p>
+                  </div>
+                  <div class="card-footer text-center">
+                     <button type="button" class="btn btn-secondary">Commander</button>
+                  </div>
+               </div>
+            </div>`;
+   }
+   articles.innerHTML = teddies;
 };
 
 afficherTeddies()

2969faaab7abb3a8828e6992b0382e8f2bf87710
Damien Pinna <damien.pinna@gmail.com>
2020-06-07 16:24:21 +0200

    ajout de la fonction afficherCompteurPanier, ne fonctionne que sur la page panier pour le momment

--- panier.js
+++ panier.js
@@ -8,6 +8,7 @@ const getTabLocalStorage = () => {
    return tabProduitsPanier;
 }
 
+const tabObjetsLocalStorage = getTabLocalStorage();
 
 /**
  * Fonction qui permet de créer une ligne du panier en HTML.
@@ -60,6 +61,16 @@ const afficherTabPanier = teddies => {
    total.innerText = `$${prixTotalDuPanier}`;
 }
 
-const tabObjetsLocalStorage = getTabLocalStorage();
-
 afficherTabPanier(tabObjetsLocalStorage);
+
+
+/**
+ * Fonction qui permet d'afficher à coté de l'icône panier la quantité d'articles se trouvant dans le panier.
+ */
+const afficherCompteurPanier = () => {
+   const compteurPanier = tabObjetsLocalStorage.reduce((acc, teddy) => acc + parseInt(teddy.quantite), 0);
+   const badge = document.querySelector('.badge');
+   badge.innerHTML = compteurPanier;
+}
+
+afficherCompteurPanier();

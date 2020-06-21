5a59baff2e87f96136991346dd8405d697a71b66
Damien Pinna <damien.pinna@gmail.com>
2020-06-21 12:18:26 +0200

    modification du commentaire de la fonction get

--- javascript/queries.js
+++ javascript/queries.js
@@ -23,14 +23,15 @@ const getTabSessionStorage = () => {
 /**
  * Fonction qui permet d'effectuer des requêtes GET à la base de données.
  * @param {String} url
+ * @returns {Array} un tableau d'objets si la requête a retourné la réponse attendue (statut HTTP 200) ou un tableau vide si la réponse est incorrect (HTTP statut entre 300 et 599).
  */
 const get = async (url) => {
    try {
       const response = await fetch(url);
-      if (response.ok) { //On test si oui ou non nous avons eu un retour correct (statut HTTP entre 200 et 299)
-         const data = response.json(); //On assigne la réponse convertit en objet JavaScript à la variable data.
+      if (response.ok) {
+         const data = response.json();
          return data;
-      } else { //Si la réponse de la requête est incorrect (statut HTTP entre 300 et 599)
+      } else {
          console.error("retour du serveur : ", response.status);
          const alert = document.querySelector('.alert');
          alert.parentElement.classList.remove('d-none');
